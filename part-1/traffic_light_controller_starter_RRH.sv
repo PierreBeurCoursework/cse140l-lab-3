86d6c5337e51ecd2c65e465ac6d80eb7af935b6c
PierreBeur <cbeurtheret@ucsd.edu>
2022-07-20 13:01:16 -0700

    Whitespace formatting

--- part-1/traffic_light_controller_starter_RRH.sv
+++ part-1/traffic_light_controller_starter_RRH.sv
@@ -13,8 +13,8 @@ module traffic_light_controller(
   output colors ew_str_light, ew_left_light, ns_light);     // traffic lights, east-west straight, east-west left, north-south
 
 // HRR = red-red following YRR; RRH = red-red following RRY;
-// ZRR = 2nd cycle yellow, follows YRR, etc. 
-  typedef enum {GRR, YRR, ZRR, HRR, RGR, RYR, RZR, RHR, RRG, RRY, RRZ, RRH} tlc_states;  
+// ZRR = 2nd cycle yellow, follows YRR, etc.
+  typedef enum {GRR, YRR, ZRR, HRR, RGR, RYR, RZR, RHR, RRG, RRY, RRZ, RRH} tlc_states;
   tlc_states    present_state, next_state;
   integer ctr5, next_ctr5,       //  5 sec timeout when my traffic goes away
           ctr10, next_ctr10;     // 10 sec limit when other traffic presents
@@ -23,50 +23,50 @@ module traffic_light_controller(
 // combinational part will reset or increment the counters and figure out the next_state
   always_ff @(posedge clk)
     if(reset) begin
-	  present_state <= RRH;        // so that EWS has top priority after reset
+      present_state <= RRH;        // so that EWS has top priority after reset
       ctr5          <= 0;
       ctr10         <= 0;
-    end  
-	else begin
-	  present_state <= next_state;
+    end
+    else begin
+      present_state <= next_state;
       ctr5          <= next_ctr5;
       ctr10         <= next_ctr10;
-    end  
+    end
 
 // combinational part of state machine ("C1" block in the Harris & Harris Moore machine diagram)
 // default needed because only 6 of 8 possible states are defined/used
   always_comb begin
     next_state = HRR;            // default to reset state
-    next_ctr5  = 0; 
+    next_ctr5  = 0;
     next_ctr10 = 0;
     case(present_state)
 /* ************* Fill in the case statements ************** */
 /* suggestion: I used a 5-counter and a 10-counter to time my green states
      if the 10-counter > a particular threshold & conflicting traffic present --> yellow
-	 else if 5-counter > a particular threshold & my own traffic disappeared --> yellow
-	 increment 5-counter whenever my own traffic is absent
-	 increment 10-counter whenever I have a green light
+     else if 5-counter > a particular threshold & my own traffic disappeared --> yellow
+     increment 5-counter whenever my own traffic is absent
+     increment 10-counter whenever I have a green light
 */
-	  GRR: begin 
+	  GRR: begin
          // when is next_state GRR? YRR?
          // what does ctr5 do? ctr10?
-      end  
-     // etc. 
+      end
+     // etc.
     endcase
   end
 
 // combination output driver  ("C2" block in the Harris & Harris Moore machine diagram)
   always_comb begin
     ew_str_light = red;                // cover all red plus undefined cases
-	ew_left_light = red;
-	ns_light = red;
+    ew_left_light = red;
+    ns_light = red;
     case(present_state)      // Moore machine
-      GRR:     ew_str_light = green;
-	  YRR,ZRR: ew_str_light = yellow;  // my dual yellow states -- brute force way to make yellow last 2 cycles
-	  RGR:     ew_left_light = green;
-	  RYR,RZR: ew_left_light = yellow;
-	  RRG:     ns_light = green;
-	  RRY,RRZ: ns_light = yellow;
+      GRR:      ew_str_light = green;
+      YRR,ZRR:  ew_str_light = yellow;  // my dual yellow states -- brute force way to make yellow last 2 cycles
+      RGR:     ew_left_light = green;
+      RYR,RZR: ew_left_light = yellow;
+      RRG:          ns_light = green;
+      RRY,RRZ:      ns_light = yellow;
     endcase
   end
 
